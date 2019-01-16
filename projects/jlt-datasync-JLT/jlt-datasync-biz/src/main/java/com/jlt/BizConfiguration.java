54e1cf1bc7b47788c7b22f0a62405a1372eeb156
jianbo.zhang01 <jianbo.zhang01@ucarinc.com>
2019-01-16 10:41:45 +0800

    delete projects

--- projects/jlt-datasync-JLT/jlt-datasync-biz/src/main/java/com/jlt/BizConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.jlt;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
-public class BizConfiguration {
-}
